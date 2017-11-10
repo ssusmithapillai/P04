960bee97c9342196e29b5f6a7a753ec536b75483
162291J SRI SUSMITHA D/O RAJKUMAR PILLAI <162291J@sit.nyp.edu.sg>
2017-11-10 10:19:50 +0800

    AddMathSub

--- MathAdd.java
+++ MathAdd.java
@@ -4,5 +4,6 @@ public class MathAdd {
 	
 	static public int add(int a, int b) {   
         return a + b; 
+        system.out.println (a+b);
 	}
 }
---
+++ MathSub.java
@@ -0,0 +1,9 @@
+public class MathSub {
+	
+	public MathSub {}
+	
+	static public int sub(int a, int b) {   
+        return a - b; 
+        system.our.println(a-b);
+	}
+}
